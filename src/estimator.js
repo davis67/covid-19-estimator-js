4c0ea7d84e8b9b77fb442f963c74d2f34fb307f1
davis67 <davisag67@gmail.com>
2020-04-08 10:23:41 +0300

    challenge-1 set up

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,11 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  const impact = {};
+  const severeImpact = {};
+
+  impact.currentlyInfected = data.reportedCases * 10;
+  severeImpact.currentlyInfected = data.reportedCases * 50;
+
+  return { data, impact, severeImpact };
+};
 
 export default covid19ImpactEstimator;
