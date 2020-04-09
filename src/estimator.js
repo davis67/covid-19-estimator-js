da92bade885f7db049e15332cfaaf2bed4a46fd7
davis67 <davisag67@gmail.com>
2020-04-09 18:01:19 +0300

    quick fix

--- src/estimator.js
+++ src/estimator.js
@@ -1,9 +1,6 @@
 class HelperEstimator {
-  constructor({
-      periodType = 'days',
-      timeToElapse,
-      reportedCases
-    },
+  constructor(
+    { periodType = 'days', timeToElapse, reportedCases },
     impactFactor
   ) {
     this.periodType = periodType;
@@ -43,11 +40,17 @@ class HelperEstimator {
 
 // impact cases
 const impactCases = (data) => {
-  const infectionsByRequestedTime = new HelperEstimator(data, 10).infectionByRequestedTime();
+  const infectionsByRequestedTime = new HelperEstimator(
+    data,
+    10
+  ).infectionByRequestedTime();
   const currentlyInfected = new HelperEstimator(data, 10).currentlyInfected();
-  const severeCasesByRequestedTime = Math.floor(infectionsByRequestedTime * 0.15);
+  const severeCasesByRequestedTime = Math.floor(
+    infectionsByRequestedTime * 0.15
+  );
   const availableHospitalBeds = Math.floor(data.totalHospitalBeds * 0.35);
-  const hospitalBedsByRequestedTime = availableHospitalBeds - severeCasesByRequestedTime;
+  const hospitalBedsByRequestedTime =
+    availableHospitalBeds - severeCasesByRequestedTime;
 
   return {
     infectionsByRequestedTime,
@@ -59,11 +62,17 @@ const impactCases = (data) => {
 
 // severe impact cases
 const severeImpactCases = (data) => {
-  const infectionsByRequestedTime = new HelperEstimator(data, 10).infectionByRequestedTime();
+  const infectionsByRequestedTime = new HelperEstimator(
+    data,
+    10
+  ).infectionByRequestedTime();
   const currentlyInfected = new HelperEstimator(data, 10).currentlyInfected();
-  const severeCasesByRequestedTime = Math.floor(infectionsByRequestedTime * 0.15);
+  const severeCasesByRequestedTime = Math.floor(
+    infectionsByRequestedTime * 0.15
+  );
   const availableHospitalBeds = Math.floor(data.totalHospitalBeds * 0.35);
-  const hospitalBedsByRequestedTime = availableHospitalBeds - severeCasesByRequestedTime;
+  const hospitalBedsByRequestedTime =
+    availableHospitalBeds - severeCasesByRequestedTime;
   return {
     infectionsByRequestedTime,
     currentlyInfected,
